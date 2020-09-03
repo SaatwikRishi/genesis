aa7221c9e6c2e253fd23abb13aeaae32a66a8cb1
SaatwikRishi <saatwiksanjay@gmail.com>
2020-09-03 10:38:40 +0530

    v.10

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:genesis/generated_plugin_registrant.dart';
+import 'package:genesis/providers/new_user_provider.dart';
+import 'package:genesis/screens/new_user.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget `is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider.value(value: NewUserInfo())],
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {},
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Newuser(),
+      ),
+    );
+  }
+}
